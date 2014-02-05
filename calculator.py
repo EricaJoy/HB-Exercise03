a0e67335a59f57e38f7ba69b643ac2932c3d18f5
Hackbright Student <info@hackbrightacademy.com>
2014-02-05 15:57:43 -0800

    The calculator does some stuff but is kind of broken. Committing to save my place while I learn about the order in which things happen.

--- calculator.py
+++ calculator.py
@@ -1,25 +1,36 @@
 import arithmetic
 
+def basic(arg1, arg2, arg3):
+    operators = {
+        "+": arithmetic.add,
+        "-": arithmetic.subtract,
+        "*": arithmetic.multiply,
+        "pow": arithmetic.power,
+        "mod": arithmetic.mod
+    }
+
+    print operators[arg1](int(arg2),int(arg3))
 
 while True:
     userdata = raw_input(">")
     calc = userdata.split(" ")
 
-    if calc[0] == "q":
-        break
-    elif calc[0] == "+":
-        print arithmetic.add(int(calc[1]),int(calc[2]))
-    elif calc[0] == "-":
-        print arithmetic.subtract(int(calc[1]),int(calc[2]))
-    elif calc[0] == "*":
-        print arithmetic.multiply(int(calc[1]),int(calc[2]))
+    if calc[0] == "+" or "-" or "*" or "pow" or "mod":
+        basic(calc[0], calc[1], calc[2])
+
     elif calc[0] == "/":
         print arithmetic.divide(float(calc[1]),float(calc[2]))
+
     elif calc[0] == "square":
         print arithmetic.square(int(calc[1]))
+
     elif calc[0] == "cube":
         print arithmetic.cube(int(calc[1]))
-    elif calc[0] == "pow":
-        print arithmetic.power(int(calc[1]),int(calc[2]))
-    elif calc[0] == "mod":
-        print arithmetic.mod(int(calc[1]),int(calc[2]))
+
+    elif calc[0] == "q":
+        break
+
+    else:
+        print "That's not a valid function. Valid functions are"
+        "+, -, *, /, mod, square, cube, pow, or q."
+
