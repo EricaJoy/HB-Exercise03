1cb8b031c2df0d57b8708829deda23114844cf38
Hackbright Student <info@hackbrightacademy.com>
2014-02-05 14:59:46 -0800

    First commit of calculator.py. Base logic added but non-functional.

---
+++ calculator.py
@@ -0,0 +1,25 @@
+import arithmetic
+
+
+while True:
+    userdata = raw_input(">")
+    calc = userdata.split(" ")
+
+    if calc[0] == "q":
+        break
+    elif calc[0] == "+":
+        pass
+    elif calc[0] == "-":
+        pass
+    elif calc[0] == "*":
+        pass
+    elif calc[0] == "/":
+        pass
+    elif calc[0] == "square":
+        pass
+    elif calc[0] == "cube":
+        pass
+    elif calc[0] == "pow":
+        pass
+    elif calc[0] == "mod":
+        pass
