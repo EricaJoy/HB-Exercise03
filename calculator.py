faf46400629a2cd010c3e6a7e246d81c71bbd1b1
Hackbright Student <info@hackbrightacademy.com>
2014-02-05 16:43:06 -0800

    Now with more error checking. Need to fix line length on calculator.py. adding arithmetic.py for great good.

--- calculator.py
+++ calculator.py
@@ -1,10 +1,16 @@
 import arithmetic
 
+# basic function takes 3 arguments
+# arg1, arg2, and arg3 are the tokenized items from raw input
+# operators is a dictionary that maps tokenized raw input operator
+# to arithmetic function.
+
 def basic(arg1, arg2, arg3):
     operators = {
         "+": arithmetic.add,
         "-": arithmetic.subtract,
         "*": arithmetic.multiply,
+        "/": arithmetic.divide,
         "pow": arithmetic.power,
         "mod": arithmetic.mod
     }
@@ -15,11 +21,11 @@ while True:
     userdata = raw_input(">")
     calc = userdata.split(" ")
 
-    if calc[0] == "+" or "-" or "*" or "pow" or "mod":
-        basic(calc[0], calc[1], calc[2])
-
-    elif calc[0] == "/":
-        print arithmetic.divide(float(calc[1]),float(calc[2]))
+    if calc[0] == "+" or calc[0] == "-" or calc[0] == "*" or calc[0] == "/" or calc[0] == "pow" or calc[0] == "mod":
+        if len(calc) == 3 and str(calc[1]).isdigit() == True and str(calc[2]).isdigit() == True:
+            basic(calc[0], calc[1], calc[2])
+        else:
+            print "Please provide two numbers to calculate."
 
     elif calc[0] == "square":
         print arithmetic.square(int(calc[1]))
@@ -31,6 +37,6 @@ while True:
         break
 
     else:
-        print "That's not a valid function. Valid functions are"
-        "+, -, *, /, mod, square, cube, pow, or q."
+        print "That's not a valid function. Valid functions are \
++, -, *, /, mod, square, cube, pow, or q."
 
